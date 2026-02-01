9b14f20651178ac82cdc9637326765b204321e79
Daniil <bojnakdaniil@gmail.com>
2026-02-01 17:32:37 +0300

    Lab2

---
+++ labs/Lab2.cs
@@ -0,0 +1,21 @@
+﻿class Lab2
+{
+    static void Main()
+    {
+        byte number; int result = 0;
+        
+        Console.Write("Enter a number: ");
+        number = byte.Parse(Console.ReadLine());
+        
+        if (number < 1 || number > 8)
+        {
+            Console.WriteLine("Invalid input");
+            return;
+        }
+        
+        result = (((number * 10) + 9) * 10) + (9 - number);
+        Console.WriteLine(result);
+    }
+}
+
+// Ссылка на задачу: https://acmp.ru/index.asp?main=solution&id_task=4
